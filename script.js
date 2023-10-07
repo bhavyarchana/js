4cd6cedf24312c1dba1e98bfae42b052654bb16a
bhavyarchana <144515351+bhavyarchana@users.noreply.github.com>
2023-10-07 15:30:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+// console.log('Hi');
+// // alert('Hello and welcome');
+
+// // variable
+// var a=9;
+// console.log(a);   //block 1
+
+// {
+//     var a=20;
+//     console.log(a);  //block 2
+// }
+// let b=23;
+// console.log(b);
+// {
+//    let b=34;
+//    console.log(b);
+// }
+// console.log(b);
+// //const
+// const t=30;
+// console.log(t);
+// {
+//     const t=90;
+//     console.log(t);
+// }
+// console.log(t);
+// //Data Types
+// var nam='bhavya';
+// console.log(typeof(nam));
+// var nam='enjoy programing';
+// console.log(typeof(nam));
+// //Number
+// var count=10;
+// console.log(typeof(count));
+// //Boolean
+// var bb=true;
+// console.log(typeof(bb));
+// var gtype;
+// console.log(typeof(gtype));
+// // Arrays
+// let ar=['red','blue','white',10];
+// console.log(ar[3]);
+// console.log(ar.length);
+// ar.pop();
+// console.log(ar);
+// // Javascript Objects
+// let car={
+//     mileage:100,
+//     model:'i10',
+//     color:'red'
+// }
+// console.log(car);
+// console.log(car.model);
+// console.log(car.mileage);
+// let person=new Object();
+// person.gender='male';
+// person.height=180;
+// console.log(person);
+
+// arithemetic Operators
+// var A=4, B=3;
+// console.log(A+B);
+// console.log(A-B);
+// console.log(A*B);
+// console.log(A/B);
+// console.log(A%B);
+
+
+// // If else 
+// c='10';
+// d='10';
+// if (c===d) {
+//     console.log('c is equal to d');
+// } else {
+//     console.log('c is not equal to d ');
+// }
+//  a=20,b=30;
+//  if (a>b && a==20) {
+//     console.log('a is greater than b and a is equal to b');
+//  } else {
+//    console.log('a is smaller than b and a is not equal to b'); 
+//  }
+//  a=20,b=30;
+//  if (a>b || a==20) {
+//     console.log('a is greater than b and a is equal to b');
+//  } else {
+//    console.log('a is smaller than b and a is not equal to b'); 
+//  }
+
+//  let add=6;
+//   add -= 3;
+//  console.log(add);
+//  Bitwise Opreators
+let a=10,b=34,c=70,d=12;
+// console.log(add);
+var exp=a+b*(c-d)-7;
+console.log(exp);
+
+const arr=2[
+{
+    name:"maya"
+},{
+    name:"krishna"
+}
+]
+// functions
+function display(name){
+    console.log('hi '+name+' welcome to js');
+}
+display('bhavya');  
+function addition(t,s){
+ var result=t+s;
+  return result;
+}
+ var result=addition(10,15);
+ console.log('the sum is '+result);
+
+// subtraction
+ function sub(j,m){
+   var result=j-m;
+   return result;
+ }
+ var result=sub(14,7);
+ console.log('the subtraction is '+result);
+//   multiplication
+function multi(a,v){
+    var result=a*v;
+    return result;
+}
+ var result=multi(14,7);
+ console.log('the product is '+result)
+
